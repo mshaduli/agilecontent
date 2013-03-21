0bcc0738a5da2bca3992db7e9971cf8bbb0c07b0
Zuhair Naqvi <hi.zuhair@gmail.com>
2013-03-21 17:03:03 +1100

    annotations implemented

---
+++ src/TNE/OperatorBundle/Annotation/ATDW/PhysicalAddress.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TNE\OperatorBundle\Annotation\ATDW;
+
+/**
+ * Description of Product
+ * 
+ * @author zuhairnaqvi
+ * 
+ * @Annotation
+ */
+class Longitude {    
+    public static function getXpathString(){
+        return '/atdw_data_results/product_distribution[$index]/product_address/row/geocode_gda_longitude';
+    }
+}
+
