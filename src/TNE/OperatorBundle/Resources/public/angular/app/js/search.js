c20827823f5cf126bf864c36fcfb4cbade176915
Charlie McKenzie <charlie@dreamoftrees.com>
2013-05-07 13:35:18 +1000

    Design fix for bolt icon on cards.

--- src/TNE/OperatorBundle/Resources/public/angular/app/js/search.js
+++ src/TNE/OperatorBundle/Resources/public/angular/app/js/search.js
@@ -138,7 +138,7 @@ SearchApp.directive('resultsList', function(){
                             '<div class="content-group"><span class="label-important">1</span> Night from <span class="price pull-right label-important">${[{ operator.min_rate }]}</span></div>' +
                             '<div class="divider"></div>' +
                             '<div class="content-group">' +
-                                '<span>Falls Creek</span> <span class="pull-right"><i class="icon-bolt"></i></span>' +
+                                '<span>Falls Creek</span> <span class="pull-right"><i class="icon-shock"></i></span>' +
                                 '<span>Bed and Breakfast</span> <span class="price pull-right">{[{ operator.distance | number:2 | distance }]}</span>' +
                             '</div>' +
                             '<div>' +
@@ -384,7 +384,7 @@ function createMarker(operator) {
                 '</div>' +
                 '<div class="content">' +
                     '<div class="content-group">' +
-                        '<span>Falls Creek</span> <span class="pull-right"><i class="icon-bolt"></i></span>' +
+                        '<span>Falls Creek</span> <span class="pull-right"><i class="icon-shock"></i></span>' +
                         '<span>Bed and Breakfast</span> <span class="price pull-right">'+ operator.distance +'km</span>' +
                     '</div>' +
                     '<div>' +
