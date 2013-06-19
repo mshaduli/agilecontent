c71bef3f117025d8245e65ba7c7d6137c30f0f8d
Srinivasan Kumar <srini@comogroupap.com>
2013-06-19 19:12:07 +0530

    Search Completed need to be tested

--- src/TNE/OperatorBundle/Entity/AccommodationRoomCalendar.php
+++ src/TNE/OperatorBundle/Entity/AccommodationRoomCalendar.php
@@ -24,6 +24,11 @@ class AccommodationRoomCalendar
      */
     private $rate;
     
+    /**
+     * @var integer
+     */
+    private $accommodation_room_id ;
+    
     private $available = true;
     
     private $room;
@@ -91,6 +96,12 @@ class AccommodationRoomCalendar
     public function setAvailable($available) {
         $this->available = $available;
     }
+    
+    public function getAccomadationRoomId()
+    {
+        return $this->getRoom()->getId();
+    }
+
     public function getRoom() {
         return $this->room;
     }
