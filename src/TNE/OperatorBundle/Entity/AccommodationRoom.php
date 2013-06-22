68ab61d1e7878e72448ec84ccdd3f8162575dc9d
Ramkumar <ramkumar@comogroupap.com>
2013-06-22 17:01:07 +0530

    room Image

--- src/TNE/OperatorBundle/Entity/AccommodationRoom.php
+++ src/TNE/OperatorBundle/Entity/AccommodationRoom.php
@@ -4,6 +4,7 @@ namespace TNE\OperatorBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * AccommodationRoom
@@ -37,13 +38,18 @@ class AccommodationRoom
      */
     private $rateTo;
 
-    private $media;    
+    private $temp;
+     
+    public $path;
+    
+    public $file;
+
     
     private $dates;
     
     public function __construct() {
         $this->dates = new ArrayCollection();
-        $this->media = new ArrayCollection();
+//        $this->media = new ArrayCollection();
     }    
 
      /**
@@ -203,23 +209,101 @@ class AccommodationRoom
         $this->id = $id;
     }
     
-    public function getMedia()
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
     {
-        return $this->media;
+        return $this->file;
     }
     
-    public function setMedia($media)
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
     {
-        $this->media = $media;
-    }    
-    
-    public function addMedia($media){
-        $media->setAccommodationRoom($this);
-        $this->media->add($media);        
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->path)) {
+            // store the old name to delete after the update
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
     }
-    
-    public function removeMedia($media){
-        $this->media->remove($media);
-    } 
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null != $this->getFile()) {
+            // do whatever you want to generate a unique name
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->path = $filename.'.'.$this->getFile()->guessExtension();
             
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+//        echo $this->path;
+        $this->getFile()->move($this->getUploadRootDir(), $this->path);
+        if ($this->temp) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/rooms';
+    }
+    
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }  
 }
