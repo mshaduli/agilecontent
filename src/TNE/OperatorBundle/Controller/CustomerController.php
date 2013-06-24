37b79dee7e7a7be7fb9b0e190d218bb34bde2162
Srinivasan Kumar <srini@comogroupap.com>
2013-06-24 16:30:52 +0530

    booking controller ready

---
+++ src/TNE/OperatorBundle/Controller/CustomerController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace TNE\OperatorBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use TNE\OperatorBundle\Entity\Customer;
+use TNE\OperatorBundle\Form\CustomerType;
+
+/**
+ * Customer controller.
+ *
+ */
+class CustomerController extends Controller
+{
+    /**
+     * Lists all Customer entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('TNEOperatorBundle:Customer')->findAll();
+
+        return $this->render('TNEOperatorBundle:Customer:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+
+    /**
+     * Finds and displays a Customer entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TNEOperatorBundle:Customer')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Customer entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('TNEOperatorBundle:Customer:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to create a new Customer entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Customer();
+        $form   = $this->createForm(new CustomerType(), $entity);
+
+        return $this->render('TNEOperatorBundle:Customer:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new Customer entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Customer();
+        $form = $this->createForm(new CustomerType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('booking_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('TNEOperatorBundle:Customer:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Customer entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TNEOperatorBundle:Customer')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Customer entity.');
+        }
+
+        $editForm = $this->createForm(new CustomerType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('TNEOperatorBundle:Customer:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Customer entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TNEOperatorBundle:Customer')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Customer entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new CustomerType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('booking_edit', array('id' => $id)));
+        }
+
+        return $this->render('TNEOperatorBundle:Customer:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Customer entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('TNEOperatorBundle:Customer')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Customer entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('booking'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
